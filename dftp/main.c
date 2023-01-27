1c712907848294f75c33b5896e4c200f893b8077
David <github@davidepalma.it>
2023-01-27 20:25:52 +0100

    initial commit

---
+++ dftp/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Ciao, sono david's ftp client");
+    return 0;
+}
---
+++ dftpd/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Ciao, sono david's ftp daemon\n");
+    return 0;
+}
